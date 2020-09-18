ea4a9a2781849e28092fd42ed21d2a93268b35f8
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-09-18 22:03:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.aaryanBhogale;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int count = 6;
+	while (count != 6) {
+        System.out.println("Count value is " + count);
+        count ++;
+    }
+     for (int i=6; i != 6 ;i++) {
+         System.out.println("Count value is " + count);
+
+     }
+    }
+}
